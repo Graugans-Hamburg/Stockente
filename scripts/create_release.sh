931f897631b045b045ec74608e13611abe6e8062
Matthias B <brett_vorm_kopf@gmx.de>
2018-01-31 19:15:27 +0100

    Repaird the release script.

--- scripts/create_release.sh
+++ scripts/create_release.sh
@@ -4,15 +4,16 @@
 
 #Step 1:Ask the user which version shall be created
 clear
-echo 'Please enter the version you would like to create (e.g. v1.4)'
+echo 'Please enter the version you would like to create (e.g. v1.2.1)'
 read version
-echo "Die folgende Version wird erstellt: Stockente_$version"
+echo "Die folgende Version wird erstellt: Stockente_${version}"
 
 ##
 echo 'Step 2:Erstelle einen neuen Ordner auf dem Desktop mit dem Namen der Programmversion'
-Zielordner="$HOME/Desktop/Stockente_$version"
-Zielordner_Windows="$HOME/Desktop/Stockente_Windows_$version"
+Zielordner="$HOME/Desktop/Stockente_${version}"
+Zielordner_Windows="$HOME/Desktop/Stockente_${version}_Windows"
 mkdir $Zielordner
+mkdir $Zielordner_Windows
 
 ##
 echo 'Step 3:Kopiere alle help Dateien in den Zielordner'
@@ -28,17 +29,14 @@ echo 'Step 5:Kopiere die zusätzlichen Recourcen in den Zielordner'
 cp -R ../pic $Zielordner
 cp -R ../pic $Zielordner_Windows
 
-##
-#echo 'Step 6: Kopiere das configure.sh in den Zielordner'
-#cp ./configure.sh $Zielordner
 
 ##
-echo 'Step 7: Das ganze Verzeichnis packen'
+echo 'Step 6: Das ganze Verzeichnis packen'
 cd $Zielordner
 cd ..
-zip -r "Stockente_$version".zip "Stockente_$version"
+zip -r "Stockente_${version}".zip "Stockente_${version}"
 cd $Zielordner_Windows
 cd ..
-zip -r "Stockente_Windows_$version".zip "Stockente_Windows_$version"
+zip -r "Stockente_${version}_Windows".zip "Stockente_${version}_Windows"
 ##
 echo 'Fertig'
